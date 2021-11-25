2e6616fd3edd579295acffc8ac6a0c945a6a6086
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-11-25 12:13:49 +0000

    making the navbar responsive, changing the seach button colour

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Search from "./components/Search";
 import Home from "./components/Home";
 import DataFetching from "./components/DataFetching";
-import { Button, Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import logo from "./ss-logo-bar.png";
@@ -14,14 +14,18 @@ export default function App() {
       <Navbar className="nav" fixed="top" expand="lg">
         <Container fluid>
           <Navbar.Brand href="/">
-            <img src={logo} class="logo img-fluid" alt="My logo" />{" "}
+            <img
+              src={logo}
+              alt="sneaker seeker logo"
+              class="logo img-fluid" 
+            />
           </Navbar.Brand>
           {/* <Navbar.Toggle aria-controls="navbarScroll" /> */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           {/* <Navbar.Collapse id="navbarScroll"> */}
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto my-2 my-lg-0"></Nav>
-              <Nav.Link href="/search">SEARCH</Nav.Link>
+              <Nav.Link href="/search" className="search-link">SEARCH</Nav.Link>
           </Navbar.Collapse>
         </Container>
       </Navbar>
